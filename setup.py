80884a47d7ab5d035a8ea554972123f7db39b04c
AlexLisnitski <aleksandr.lisnitskiy@ringteam.com>
2020-08-13 13:51:50 +0300

    Fix setup links and add bootloader specification

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name="cc3200tool",
-    version="0.1.0",
-    description="A tool to upload files to TI CC3200",
+    version="0.1.1",
+    description="A tool for CC32xx TI micro firmware update",
     author="Kiril Zyapkov",
     author_email="k.zyapkov@allterco.com",
-    url="http://github.com/allterco/cc3200tool",
+    url="http://github.com/AlexLisnitski/cc3200tool",
     packages=['cc3200tool'],
-    package_data={'cc3200tool': ['dll/*.dll']},
+    package_data={'cc3200tool': ['dll/*.dll', 'dll/gen2/*.ptc']},
     entry_points = {
         'console_scripts': ['cc3200tool=cc3200tool.cc:main'],
     },
